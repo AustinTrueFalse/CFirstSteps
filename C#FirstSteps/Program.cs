625dbcbe7b82a87c80e11bfd09366a436570d59f
Maksim <craash00077@gmail.com>
2023-09-16 09:13:09 +0300

    Form

--- C#FirstSteps/Program.cs
+++ C#FirstSteps/Program.cs
@@ -2,8 +2,15 @@
 {
     private static void Main(string[] args)
     {
-        string MyName;
-        MyName = "Jane";
-        Console.WriteLine(MyName);
+        string MyName = "Максим";
+        byte Age = 26;
+        bool Pet = false;
+        float ShoeSize = 44;
+
+        Console.WriteLine("Меня зовут {0}", MyName);
+        Console.WriteLine("My age is {0}", Age);
+        Console.WriteLine("Do I have a pet? {0}", Pet);
+        Console.WriteLine("My shoe size is {0}", ShoeSize);
+
     }
 }

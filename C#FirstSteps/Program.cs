21a62e292438ec40e4cce35d9545d4dbf983549e
Maksim <craash00077@gmail.com>
2023-10-04 21:40:55 +0300

    Задание 5.5.3

--- C#FirstSteps/Program.cs
+++ C#FirstSteps/Program.cs
@@ -1,21 +1,44 @@
-﻿using System.Xml.Linq;
+﻿using System;
+using System.Drawing;
+using System.Globalization;
+using System.Reflection.Metadata;
+using System.Reflection.Metadata.Ecma335;
+using System.Xml.Linq;
 
 internal class Program
 {
-    private static void Main(string[] args)
+    static void Main(string[] args)
     {
 
-        int A = 5;
-        int B = 10;
+        Console.WriteLine("Напишите что-то");
 
-        var res = A == B;
+        var str = Console.ReadLine();
 
-        Console.WriteLine(res);
+        Console.WriteLine("Укажите глубину эха");
+
+        var deep = int.Parse(Console.ReadLine());
+
+        Echo(str, deep);
+    }
+
+    static void Echo(string saidword, int deep)
+    {
+        var modif = saidword;
+
+        if (modif.Length > 2) {
+            modif = modif.Remove(0, 2);
+        };
+
+        Console.WriteLine("..." + modif);
+
+
+        if (deep > 1)
+        {
+            Echo(modif, deep - 1);
+        }
 
 
 
 
     }
-
-
 }

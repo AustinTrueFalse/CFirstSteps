baf49259cdd207617f25dfe27ac907894b866ce8
Maksim <craash00077@gmail.com>
2023-10-06 20:53:48 +0300

    Итоговый проект 5.6.1 (HW-03)

--- C#FirstSteps/Program.cs
+++ C#FirstSteps/Program.cs
@@ -9,36 +9,108 @@ internal class Program
 {
     static void Main(string[] args)
     {
+        (string name, string lastname, int age, string[] petsArray, string[] colorsArray) User;
 
-        Console.WriteLine("Напишите что-то");
+        Console.WriteLine("Введите имя");
 
-        var str = Console.ReadLine();
+        User.name = Console.ReadLine();
 
-        Console.WriteLine("Укажите глубину эха");
+        Console.WriteLine("Введите фамилию");
 
-        var deep = int.Parse(Console.ReadLine());
+        User.lastname = Console.ReadLine();
 
-        Echo(str, deep);
-    }
+        User.age = checkCount("Age");
 
-    static void Echo(string saidword, int deep)
-    {
-        var modif = saidword;
+        Console.WriteLine("У вас есть питомец? (Y/N)");
 
-        if (modif.Length > 2) {
-            modif = modif.Remove(0, 2);
-        };
+        var hasPet = Console.ReadLine();
 
-        Console.WriteLine("..." + modif);
-
-
-        if (deep > 1)
+        if (hasPet == "Y")
         {
-            Echo(modif, deep - 1);
+            var tempPet = checkCount("Pet");
+
+            User.petsArray = new string[tempPet];
+
+            Console.WriteLine("Введите имена питомцев");
+
+            for (int i = 0; i < User.petsArray.Length; i++)
+            {
+                User.petsArray[i] = Console.ReadLine();
+            }
+        }
+        else
+        {
+            User.petsArray = new string[0];
         }
 
+        var tempColor = checkCount("Color");
+
+        User.colorsArray = new string[tempColor];
+
+        Console.WriteLine("Введите любимые цвета");
+
+        for (int i = 0; i < User.colorsArray.Length; i++)
+        {
+            User.colorsArray[i] = Console.ReadLine();
+        }
+
+        
+
+        
 
 
 
     }
-}
+
+    static int checkCount(string methType)
+    {
+        int checkedCount = 0;
+
+        while (checkedCount == 0)
+        {
+            if (methType == "Age")
+            {
+                Console.WriteLine("Введите возраст");
+            }
+            if (methType == "Pet")
+            {
+                Console.WriteLine("Введите кол-во питомцев");
+            }
+            if (methType == "Color")
+            {
+                Console.WriteLine("Введите кол-во цветов");
+            }
+
+            var temp = Console.ReadLine();
+
+            int checkedNumber;
+
+            checkNum(temp, out checkedNumber);
+
+            checkedCount = checkedNumber;
+        }
+
+        return checkedCount;
+    }
+
+    
+        static bool checkNum(string num, out int checkedNum)
+        {
+
+            if (int.TryParse(num, out int intnum))
+            {
+                if (intnum > 0)
+                {
+                    checkedNum = intnum;
+                    return false;
+                }
+            }
+            {
+                Console.WriteLine("Введите число больше 0");
+                checkedNum = 0;
+                return true;
+            }
+        }
+
+    }
+
